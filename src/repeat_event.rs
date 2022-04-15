6c086ce42edc6b6936fb4a66ab0e57b48c14480c
Michael Hewner <hewner@gmail.com>
2022-04-15 16:06:48 -0400

    cool closure based repeat event for tests

--- src/repeat_event.rs
+++ src/repeat_event.rs
@@ -1,4 +1,5 @@
 
+use std::marker;
 use super::event_queue::*;
 
 pub enum RepeatResult {
@@ -18,10 +19,34 @@ pub fn do_next_and_repeat<S, EQEntry:RepeatEvent<S>>(state : &mut S, queue : &mu
         RepeatResult::RescheduleFor(new_time) => {
             queue.register_event(event, new_time)
         }
-    };
-    
-            
+    };            
         
+}
+
+pub struct ClosureRepeatEvent<S, F>
+    where F: Fn(&mut S, EventTime, EventTime) -> RepeatResult
+{
+
+    _state : marker::PhantomData<S>, 
+    closure: F
+        
+}
+
+impl<S, F> ClosureRepeatEvent<S,F> where F: Fn(&mut S, EventTime, EventTime) -> RepeatResult {
+    pub fn new(f : F) -> ClosureRepeatEvent<S, F> {
+        ClosureRepeatEvent { _state : std::marker::PhantomData, closure : f }
+    }
+}
+
+impl<S, F> RepeatEvent<S> for ClosureRepeatEvent<S,F> where F: Fn(&mut S, EventTime, EventTime) -> RepeatResult {
+
+    fn do_event(&mut self,
+                state : &mut S,
+                scheduled : EventTime,
+                actual : EventTime) -> RepeatResult {
+        (self.closure)(state, scheduled, actual)
+    }
+
 }
 
 
--- src/test.rs
+++ src/test.rs
@@ -11,39 +11,51 @@ fn bad_add(a: i32, b: i32) -> i32 {
 
 #[cfg(test)]
 mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
     use crate::repeat_event::*;
     use crate::event_queue::*;
-
-    struct Adder {
-        pub val : u32
-    }
-
-    struct AdderEvent {
-        val_to_add : u32,
-        times : u32
-            
-    }
-
-    impl RepeatEvent<Adder> for AdderEvent {
-        fn do_event(&mut self, state : &mut Adder, _ : EventTime, _ : EventTime) -> RepeatResult {
-            state.val = state.val + self.val_to_add;
-            RepeatResult::Done
-        }
-    }
     
     #[test]
-    fn test_bad_add() {
-        // This assert would fire and test will fail.
-        // Please note, that private functions can be tested too!
-        //assert_eq!(bad_add(1, 2), 3);
+    fn test_invoke_once() {
 
         let mut events = EventQueue::new();
-        let mut adder  = Adder { val : 0 };
-        let mut add_event = AdderEvent { val_to_add : 3, times : 1 };
+        let mut change_me : u32 = 0;
+        let closure = |state : &mut u32, _, _| {             
+            *state = *state + 1;
+            RepeatResult::Done
+        };
+        
+        let mut add_event = ClosureRepeatEvent::new( closure );
+                
         events.register_event(add_event, 10.);
-        do_next_and_repeat(&mut adder, &mut events, 11.);
-        assert_eq!(adder.val, 3);
+        do_next_and_repeat(&mut change_me, &mut events, 11.);
+        assert_eq!(change_me, 1);        
+    }
+
+    #[test]
+    fn test_invoke_twice() {
+
+        let mut events = EventQueue::new();
+        let mut change_me : u32 = 0;
+        let closure = |state : &mut u32, _, _| {             
+            *state = *state + 1;
+            if(*state == 2) {
+                RepeatResult::Done
+            } else {
+                RepeatResult::RescheduleFor(100.)
+            }
+        };
+        
+        let mut add_event = ClosureRepeatEvent::new( closure );
+                
+        events.register_event(add_event, 10.);
+        do_next_and_repeat(&mut change_me, &mut events, 11.);
+        assert_eq!(change_me, 1);
+        do_next_and_repeat(&mut change_me, &mut events, 11.);
+        assert_eq!(change_me, 2);
+        assert_eq!(events.is_empty(), true);
         
     }
+
+
+
 }
